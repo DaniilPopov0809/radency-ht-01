be65f7331f6b1b3714ae4307d1a2e96727a4e351
Daniil Popov <x6uhrox@gmail.com>
2023-08-01 11:38:25 +0300

    ADDED: footer and header

--- src/markup/infoTableRowMarkup.js
+++ src/markup/infoTableRowMarkup.js
@@ -1,7 +1,7 @@
 import { nanoid } from "nanoid";
 
 export const InfoTableRowMarkup = (item) =>
-  `<tr class='table-row' data-id='${nanoid()}'>
+  `<tr class='table-light' data-id='${nanoid()}'>
 <td>${item.category}</td>
 <td>${item.active}</td>
 <td>${item.archived}</td>
--- src/markup/todoTableRowMarkup.js
+++ src/markup/todoTableRowMarkup.js
@@ -1,7 +1,7 @@
 import { truncateString } from "../helpers/truncateString";
 
 export const todoTableRowMarkup = (item) =>
-  `<tr class='table-row' data-id='${item.id}'>
+  `<tr class='table-light' data-id='${item.id}'>
 <td>${item.title}</td>
 <td>${item.created}</td>
 <td>${item.category}</td>
@@ -9,9 +9,13 @@ export const todoTableRowMarkup = (item) =>
 <td>${item.dates.join(", ")}</td>
 <td>
     <div>  
-        <button type="button" class="btn btn-primary" name='edit' data-id="${item.id}">Edit</button>
-        <button type="button" class="btn" name='archive'data-id="${item.id}">Archive</button>
-        <button type="button" class="btn remove-button" name='remove' data-id="${
+        <button type="button" class="btn btn-primary" name='edit' data-id="${
+          item.id
+        }">Edit</button>
+        <button type="button" class="btn btn-primary" name='archive'data-id="${
+          item.id
+        }">Archive</button>
+        <button type="button" class="btn btn-primary" name='remove' data-id="${
           item.id
         }">Remove</button>
     <div>
